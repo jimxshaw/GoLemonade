d17f98dd563f3dfaf1b4597e810569e83ffe4aed
Jim Shaw <jimxshaw@gmail.com>
2017-04-29 19:42:03 -0500

    Simplified main function.

--- src/github.com/golemonade/webapp/main.go
+++ src/github.com/golemonade/webapp/main.go
@@ -1,31 +1,9 @@
 package main
 
 import (
-	"io"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Look into request object and see what file was requested and
-		// then translate that into our file system.
-		f, err := os.Open("public" + r.URL.Path)
-
-		// Check to see if we successfully opened that file.
-		if err != nil {
-			// Write out a header.
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Println(err)
-		}
-
-		// Assuming everything succeeded, we close it.
-		defer f.Close()
-
-		// Copy the file directly to the response writer.
-		io.Copy(w, f)
-	})
-
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", http.FileServer(http.Dir("public")))
 }
