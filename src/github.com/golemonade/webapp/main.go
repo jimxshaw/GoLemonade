e598c40d252415325430caee4bd4f077ca66fe01
Jim Shaw <jimxshaw@gmail.com>
2017-04-30 12:55:26 -0500

    Added custom handlers to main func.

--- src/github.com/golemonade/webapp/main.go
+++ src/github.com/golemonade/webapp/main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	templates := populateTemplates()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Find the name of the template we're seeking. When using
+		// a template parsing method like ParseGlob, the template
+		// name is its file name.
+		// Slice the first character off the path because it will
+		// always be a slash /.
+		requestedFile := r.URL.Path[1:]
+
+		// Look up the template with the name of the requested file
+		// and also adding the suffix. The user won't have to type
+		// .html every time in the url if we include the suffix.
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+		}
+	})
+
+	// The way url resolution works in Go is that anything that comes
+	// in prefixed with img or css will be handled by these two handlers
+	// because they're more specific than the first handler above, which
+	// only listens on the root url.
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
+
+	http.ListenAndServe(":8000", nil)
 }
 
 func populateTemplates() *template.Template {
