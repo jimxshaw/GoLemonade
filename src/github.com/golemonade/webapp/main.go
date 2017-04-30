784b157582fed3a7fefe6bae92da45c566c27ddc
Jim Shaw <jimxshaw@gmail.com>
2017-04-30 18:45:20 -0500

    Added shop.go. Called it in main.

--- src/github.com/golemonade/webapp/main.go
+++ src/github.com/golemonade/webapp/main.go
@@ -22,11 +22,21 @@ func main() {
 		// always be a slash /.
 		requestedFile := r.URL.Path[1:]
 
-		context := viewmodel.NewBase()
+		template := templates[requestedFile+".html"]
 
-		t := templates[requestedFile+".html"]
-		if t != nil {
-			err := t.Execute(w, context)
+		// Initialize context to be an empty interface so that it
+		// can take any data value.
+		var context interface{}
+
+		switch requestedFile {
+		case "shop":
+			context = viewmodel.NewShop()
+		default:
+			context = viewmodel.NewBase()
+		}
+
+		if template != nil {
+			err := template.Execute(w, context)
 			if err != nil {
 				log.Println(err)
 			}
@@ -51,34 +61,46 @@ func main() {
 // template that defines the look and feel for that template.
 func populateTemplates() map[string]*template.Template {
 	result := make(map[string]*template.Template)
+
 	const basePath = "templates"
+
 	layout := template.Must(template.ParseFiles(basePath + "/_layout.html"))
+
 	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+
 	dir, err := os.Open(basePath + "/content")
 	if err != nil {
 		panic("Failed to open template blocks directory: " + err.Error())
 	}
+
 	fis, err := dir.Readdir(-1)
 	if err != nil {
 		panic("Failed to read contents of content directory: " + err.Error())
 	}
+
 	for _, fi := range fis {
 		f, err := os.Open(basePath + "/content/" + fi.Name())
 		if err != nil {
 			panic("Failed to open template '" + fi.Name() + "'")
 		}
+
 		content, err := ioutil.ReadAll(f)
 		if err != nil {
 			panic("Failed to read content from file '" + fi.Name() + "'")
 		}
+
 		f.Close()
+
 		tmpl := template.Must(layout.Clone())
+
 		_, err = tmpl.Parse(string(content))
 		if err != nil {
 			panic("Failed to parse contents of '" + fi.Name() + "' as template")
 		}
+
 		result[fi.Name()] = tmpl
 	}
+
 	return result
 }
 
