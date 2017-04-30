13204e829ec743b9807313b0efab942c007dbd87
Jim Shaw <jimxshaw@gmail.com>
2017-04-30 12:41:53 -0500

    Changed up folder structure. Added populateTemplates function.

--- src/github.com/golemonade/webapp/main.go
+++ src/github.com/golemonade/webapp/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 )
 
 func main() {
-	http.ListenAndServe(":8080", http.FileServer(http.Dir("public")))
+	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+}
+
+func populateTemplates() *template.Template {
+	// Here's the container that has all of the templates we'll
+	// be loading in.
+	result := template.New("templates")
+
+	// Location where the templates are stored on the file system.
+	const basePath = "templates"
+
+	// Parse the templates in the context of the result container.
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+
+	// All the parsed out templates will be the children of this
+	// result template container. That doesn't matter to Go as
+	// it will look through the entire template tree when given
+	// a template to execute.
+	return result
 }
